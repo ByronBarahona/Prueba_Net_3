581ceb8a9dc7efbeda93d276b5cf30de12c786b4
ByronBarahona <barahonabyron38@gmail.com>
2023-07-05 02:09:01 -0400

    Avances

---
+++ ThePlayZone/wwwroot/js/Login.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $("#login-form").submit(function (e) {
+        e.preventDefault();
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        // Realizar la validación del inicio de sesión aquí
+
+        // Ejemplo: Validación básica
+        if (username === "admin" && password === "12345") {
+            // Inicio de sesión válido, redireccionar
+            window.location.href = "/Home/Index";
+        } else {
+            // Mostrar mensaje de error
+            $(".error-message").show().text("Nombre de usuario o contraseña incorrectos.");
+        }
+    });
+});
