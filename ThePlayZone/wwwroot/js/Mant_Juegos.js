e4e5532e5e862dcdfd23a4ef691e3efcf2ca0824
ByronBarahona <barahonabyron38@gmail.com>
2023-07-05 02:44:14 -0400

    Avances de las 3 AM

---
+++ ThePlayZone/wwwroot/js/Mant_Juegos.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    // Mostrar formulario de creación al hacer clic en el botón "Agregar nuevo videojuego"
+    $("#btnAdd").click(function () {
+        $("#addForm").show();
+    });
+
+    // Ocultar formulario de creación al hacer clic en el botón "Cancelar"
+    $("#btnCancel").click(function () {
+        $("#addForm").hide();
+    });
+
+    // Enviar el formulario de creación al hacer clic en "Guardar"
+    $("#createForm").submit(function (e) {
+        e.preventDefault();
+
+        // Obtener los valores de los campos de entrada
+        var title = $("#title").val();
+        var platform = $("#platform").val();
+        var genre = $("#genre").val();
+
+        // Realizar la operación de creación (enviar datos al servidor, etc.)
+        // Ejemplo:
+        // $.post("/VideoGames/Create", { title: title, platform: platform, genre: genre })
+        //     .done(function(response) {
+        //         // Actualizar la tabla de videojuegos con el nuevo registro
+        //         // ...
+
+        //         // Ocultar formulario de creación
+        //         $("#addForm").hide();
+
+        //         // Limpiar los campos de entrada
+        //         $("#title").val("");
+        //         $("#platform").val("");
+        //         $("#genre").val("");
+        //     });
+
+        // NOTA: Para implementar el CRUD completo, debes agregar lógica similar para Leer, Actualizar y Eliminar registros.
+    });
+});
